f338cfa1068a9bc17e18262e344f40eeec40a8c0
Nils Berg <berg.nils@gmail.com>
2014-03-15 17:24:16 +0100

    set endCount so the [Timing] block is generated correctly

--- blitzloop_data.h
+++ blitzloop_data.h
@@ -29,12 +29,13 @@ struct Timing {
  * TODO: Last beat the same as last syllable start or end? Assuming from marcans files it's somewhere behind
  * Timing.endCount 
  */
-Timing UStoBLtiming(int gap, float bpm, int endCount) {
+Timing UStoBLtiming(int gap, float bpm, int endBeat) {
   Timing result;
   result.startTime = ((float) gap) / 1000.f;
   result.startCount = 0;
   float secondsPerBeat = 60.f / bpm;
-  result.endTime = ((float) endCount) * secondsPerBeat;
+  result.endTime = ((float) endBeat) * secondsPerBeat;
+  result.endCount = endBeat;
   return result;
 }
 std::string FormatsStylesVariantsDefault = "[Formats]\nL=Latin\n\n[Styles]\n{latin}\nfont=TakaoPGothic.ttf\nsize=11\noutline_width=0.3\nborder_width=0.8\ncolors=ffffff,12309A,000000\ncolors_on=12309A,ffffff,000000\n\n[Variants]\n{latin}\nname=latin\ntags=L\nstyle=latin\n\n";
