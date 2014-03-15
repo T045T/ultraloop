e02d7eca5f7d79a3a998f2a15f91c39bf6b72e84
Nils Berg <berg.nils@gmail.com>
2014-03-15 16:33:19 +0100

    add blitzloop_data.h

---
+++ blitzloop_data.h
@@ -0,0 +1,42 @@
+#ifndef BLITZLOOP_DATA_H
+#define BLITZLOOP_DATA_H
+
+struct Meta {
+  std::string title;
+  std::string artist;
+  std::string album;
+  std::string seenon;
+  std::string genre;
+  std::string lang;
+};
+
+struct Song {
+  std::string audio;
+  std::string video;
+  std::string video_offset;
+  std::string cover;
+};
+
+struct Timing {
+  float startTime;
+  int startCount;
+  float endTime;
+  int endCount;
+};
+
+/**
+ * gap is in MS, BPM is, well, BPM, endCount is the number of the last beat
+ * TODO: Last beat the same as last syllable start or end? Assuming from marcans files it's somewhere behind
+ * Timing.endCount 
+ */
+Timing UStoBLtiming(int gap, float bpm, int endCount) {
+  Timing result;
+  result.startTime = ((float) gap) / 1000.f;
+  result.startCount = 0;
+  float secondsPerBeat = 60.f / bpm;
+  result.endTime = ((float) endCount) * secondsPerBeat;
+  return result;
+}
+std::string FormatsStylesVariantsDefault = "[Formats]\nL=Latin\n\n[Styles]\n{latin}\nfont=TakaoPGothic.ttf\nsize=11\noutline_width=0.3\nborder_width=0.8\ncolors=ffffff,12309A,000000\ncolors_on=12309A,ffffff,000000\n\n[Variants]\n{latin}\nname=latin\ntags=L\nstyle=latin\n\n";
+
+#endif
